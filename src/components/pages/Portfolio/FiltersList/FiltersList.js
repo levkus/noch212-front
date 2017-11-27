4e4dac88d44e60d37353c2c81514d917ba0c3e78
Lev Ostanin <lev.ostanin@gmail.com>
2017-11-27 10:17:33 +0300

    started filters code splitting

---
+++ src/components/pages/Portfolio/FiltersList/FiltersList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import './FiltersList.css'
+
+class FiltersList extends Component {
+  render () {
+    return (
+      <div>FiltersList</div>
+    )
+  }
+}
+
+export default FiltersList
